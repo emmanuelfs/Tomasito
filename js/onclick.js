dc86b0c7191a4a40fee19a6302ee378cec33b6ee
emmanuelfs <efsantinoli@gmail.com>
2017-12-12 16:38:43 -0300

    Script source

--- js/onclick.js
+++ js/onclick.js
@@ -1,6 +1,5 @@
 $("#ABM").click(function(){
-    $.ajax({url: "ABM.txt", success: function(result){
+    $.ajax({url: "/Tomasito/content/ABM.txt", success: function(result){
         $("#ajax-info").html(result);
     }});
-	$('#ABM').hide();
 });
--- onclick.js
+++
@@ -1,6 +0,0 @@
-$("#ABM").click(function(){
-    $.ajax({url: "ABM.txt", success: function(result){
-        $("#ajax-info").html(result);
-    }});
-	$('#ABM').hide();
-});
