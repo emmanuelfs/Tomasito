38a5c5bd8d5053136aff853f9a69f5afbee5abf6
emmanuelfs <efsantinoli@gmail.com>
2017-12-18 12:40:29 -0300

    redo mobile first

--- OLD/js/onclick.js
+++ OLD/js/onclick.js
@@ -1,13 +1,13 @@
 $('#navBar').children('ul').children('li').click(function(){
 	$idClicked = $(this).children('a').attr('id');
-    $.ajax({url: "content/" + $idClicked + ".html", success: function(result){
+    $.ajax({url: "content/" + $idClicked + ".txt", success: function(result){
         $("#ajax-info").html(result);
     }});
 });
 
 $('#bottombar').click(function(){
 	$idClicked = $(this).children('a').attr('id');
-    $.ajax({url: "content/" + $idClicked + ".html", success: function(result){
+    $.ajax({url: "content/" + $idClicked + ".txt", success: function(result){
         $("#ajax-info").html(result);
     }});
 });
